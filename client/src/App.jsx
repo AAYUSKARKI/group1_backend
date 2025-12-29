af80806b49c4e03b1e3d555808fbcf28e4335e23
aayus karki <karki.aayush2003@icloud.com>
2025-12-29 16:17:00 +0545

    feat: refactor App component to implement routing and remove unused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+import Tables from './pages/Tables';
+import MenuPage from './pages/Menu';
+import POSPage from './pages/POS';
+import BillsPage from './pages/Bills';
+import ReservationsPage from './pages/Reservations';
+import SettingsPage from './pages/Settings';
+import KitchenPage from './pages/Kitchen';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/pos" element={<POSPage />} />
+        <Route path="/bills" element={<BillsPage />} />
+        <Route path="/reservations" element={<ReservationsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/kitchen" element={<KitchenPage />} />
+      </Routes>
+    </Router>
   )
 }
 
